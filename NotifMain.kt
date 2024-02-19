565d3785bb70fbfa55cbdb581972e39de70b5b51
sandeep chouhan <118365118+sandeep-12333333@users.noreply.github.com>
2024-02-19 07:41:16 +0530

    Add files via upload

---
+++ NotifMain.kt
@@ -0,0 +1,25 @@
+package com.example.notificationapplication
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class NotifMain : AppCompatActivity() {
+    lateinit var notificationManager: NotificationManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notif_main)
+        var txtView = findViewById<TextView>(R.id.tv)
+
+        var inp = Notif()
+
+        txtView.text = "Hey!!"
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.cancel(inp.notificationId)
+    }
+}
